123851f921a14ae71e8f9e17476a2e9ef1d93fa7
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-09 16:50:38 +0900

    #5.1 Container Presenter Pattern part Two (09:33)

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SearchPresenter from "./SearchPresenter";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default class extends React.Component {
+    state = {
+        movieResults: null,
+        tvResults: null,
+        searchTerm: "",
+        error: null,
+        loading: false
+    };
+
+    render() {
+        const {movieResults, tvResults, searchTerm, error, loading} = this.state;
+        return <SearchPresenter movieResults={movieResults} tvResults={tvResults} searchTerm={searchTerm} error={error} loading={loading}/>
+    };
+}
---
+++ src/Routes/TV/TVContainer.js
@@ -0,0 +1,18 @@
+ import React from "react";
+import TVPresenter from "./TVPresenter";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default class extends React.Component {
+    state = {
+        topRated: null,
+        popular: null,
+        airingToday: null,
+        error: null,
+        loading: true
+    };
+
+    render() {
+        const {topRated, popular, airingToday, error, loading} = this.state;
+        return <TVPresenter topRated={topRated} popular={popular} airingToday={airingToday} error={error} loading={loading}/>
+    };
+}
