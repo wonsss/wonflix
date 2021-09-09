981065f0ec75b3739a3aa47c1cc0df69abd7ad6f
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-09 21:22:52 +0900

    5.6 Detail Container

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,15 +1,54 @@
+import { moviesApi, tvApi } from "api";
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default class extends React.Component {
-    state = {
-        result: null,
-        error: null,
-        loading: true
-    };
+    constructor(props){
+        super(props);
+        const {
+            location: {pathname}
+        } = props;
+        this.state = {
+            result: null,
+            error: null,
+            loading: true,
+            isMovie: pathname.includes("/movie/")
+        }; 
+    }
+
+    async componentDidMount() {
+        const {
+            match: {
+                params: {id}
+            },
+            history: {push},
+            location: {pathname},
+        } = this.props;
+        const {isMovie} = this.state;
+        this.isMovie = pathname.includes("/movie/");
+        const parsedId = parseInt(id);
+        if (isNaN(parsedId)){
+            return push("/")
+        };
+        let result = null;
+        try{
+            if (isMovie) {
+                const request = await moviesApi.movieDetail(parsedId);
+                result = request.data;
+              } else {
+                const request = await tvApi.showDetail(parsedId);
+                result = request.data;
+              }  
+        }catch{
+            this.setState({error:"can't find anything."})
+        }finally{
+            this.setState({loading:false, result})
+        }
+    }
 
     render() {
+        console.log(this.state);
         const {result, error, loading} = this.state;
         return <DetailPresenter result={result} error={error} loading={loading}/>
     };
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -36,7 +36,6 @@ export default class extends React.Component {
     }
 
     render() {
-        console.log(this.state);
         const {movieResults, tvResults, searchTerm, error, loading} = this.state;
         return <SearchPresenter movieResults={movieResults} tvResults={tvResults} searchTerm={searchTerm} error={error} loading={loading} handleSubmit={this.handleSubmit}/>
     };
