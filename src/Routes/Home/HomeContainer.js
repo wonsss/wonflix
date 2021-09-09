831b24585a940d6cfe42621632100213eaf2fb72
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-09 16:23:31 +0900

    #5.0 Container Presenter Pattern part One

---
+++ src/Routes/Home/HomeContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default class extends React.Component {
+    state = {
+        nowPlaying: null,
+        upcoming: null,
+        popular: null,
+        error: null,
+        loading: true
+    };
+
+    render() {
+        const {nowPlaying, upcoming, popular, error, loading} = this.state;
+        return <HomePresenter nowPlaying={nowPlaying} upcoming={upcoming} popular={popular} error={error} loading={loading}/>
+    };
+}
