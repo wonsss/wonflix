4e38d9f3eb3d580affd5ac34dec7d6d0605ea67f
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-09 11:18:36 +0900

    #3.1 CSS in React part Two CSS모듈은 className을 임의화하여 css가 global이 아닌 local이 되게 하는 것이다. 방법은 css파일 이름을 이름.module.css로 바꿔주고, import할 때도 자바스크립트처럼 import styles from “./Header.module.css”로 해준다. 그리고 className을 자바스크립트 오브젝트처럼 해당 태그에 직접적으로 사용한다. <ul className={styles.navList}>

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Header.css"
+import styles from "./Header.module.css"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
-    <header className="nav">
-        <ul>
+    <header>
+        <ul className={styles.navList}>
             <li><a href="/">Movies</a></li>
             <li><a href="/tv">Tv</a></li>
             <li><a href="/search">Search</a></li>
