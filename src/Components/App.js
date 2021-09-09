80f7c2e2f536ba4d03aa98a4dcebaafc9b59d767
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-09 11:05:54 +0900

    #3.0 CSS in React part One

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import Router from "Components/Router";
-import Header from "Components/Header";
+import Header from "Components/Header/index";
 
 class App extends Component {
   render() {
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Header.css"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
-    <header>
+    <header className="nav">
         <ul>
             <li><a href="/">Movies</a></li>
             <li><a href="/tv">Tv</a></li>
