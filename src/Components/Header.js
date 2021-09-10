eb318269ad72721390bbcb66cdb020551e380b58
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-10 18:04:54 +0900

    #6.9 Detail Container part Two

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,50 +1,56 @@
 import React from "react";
-import {Link, withRouter} from "react-router-dom";
-import styled from 'styled-components'
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = styled.header`
-    color: white;
-    position: fixed;
-    top:0;
-    left:0;
-    width:100%;
-    height:50px;
-    display:flex;
-    align-items:center;
-    padding: 10px 0px;
-    background-color:rgba(20,20,20,0.8);
-    box-shadow: 0px 1px 5px 2px rgba(0,0,0,0.8);
-    z-index:10;
-
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: rgba(20, 20, 20, 0.8);
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+  z-index: 10;
 `;
 const List = styled.ul`
-    display:flex;
-    &:hover{
-    }
+  display: flex;
+  &:hover {
+  }
 `;
 
 const Item = styled.li`
-    width: 60px;
-    height: 50px;
-    text-align: center;
-    border-bottom: 3px solid ${props => props.current ? "#c0392b" : "transparent"};
-    transition: border-bottom .4s ease-in-out;
+  width: 60px;
+  height: 50px;
+  text-align: center;
+  border-bottom: 3px solid
+    ${(props) => (props.current ? "#c0392b" : "transparent")};
+  transition: border-bottom 0.4s ease-in-out;
 `;
 
 const SLink = styled(Link)`
-    height: 50px;   
-    display: flex;
-    align-items: center; 
-    justify-content: center;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default withRouter(({location: {pathname}}) => (
-    <Header>
-        <List>
-            <Item current={pathname === "/"}><SLink to="/">Movies</SLink></Item>
-            <Item current={pathname === "/tv"}><SLink to="/tv">Tv</SLink></Item>
-            <Item current={pathname === "/search"}><SLink to="/search">Search</SLink></Item>
-        </List>
-    </Header>
+export default withRouter(({ location: { pathname } }) => (
+  <Header>
+    <List>
+      <Item current={pathname === "/"}>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item current={pathname === "/tv"}>
+        <SLink to="/tv">Tv</SLink>
+      </Item>
+      <Item current={pathname === "/search"}>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
 ));
--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -4,50 +4,50 @@ import DetailPresenter from "./DetailPresenter";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default class extends React.Component {
-    constructor(props){
-        super(props);
-        const {
-            location: {pathname}
-        } = props;
-        this.state = {
-            result: null,
-            error: null,
-            loading: true,
-            isMovie: pathname.includes("/movie/")
-        }; 
-    }
-
-    async componentDidMount() {
-        const {
-            match: {
-                params: {id}
-            },
-            history: {push},
-            location: {pathname},
-        } = this.props;
-        const {isMovie} = this.state;
-        this.isMovie = pathname.includes("/movie/");
-        const parsedId = parseInt(id);
-        if (isNaN(parsedId)){
-            return push("/")
-        };
-        let result = null;
-        try{
-            if (isMovie) {
-                ({data: result} = await moviesApi.movieDetail(parsedId));
-              } else {
-                ({data: result} = await tvApi.showDetail(parsedId));
-              }  
-        }catch{
-            this.setState({error:"can't find anything."})
-        }finally{
-            this.setState({loading:false, result})
-        }
-    }
-
-    render() {
-        console.log(this.state);
-        const {result, error, loading} = this.state;
-        return <DetailPresenter result={result} error={error} loading={loading}/>
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
     };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+      location: { pathname },
+    } = this.props;
+    const { isMovie } = this.state;
+    this.isMovie = pathname.includes("/movie/");
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+    let result = null;
+    try {
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parsedId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(parsedId));
+      }
+    } catch {
+      this.setState({ error: "can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
+    }
+  }
+
+  render() {
+    console.log(this.state);
+    const { result, error, loading } = this.state;
+    return <DetailPresenter result={result} error={error} loading={loading} />;
+  }
 }
