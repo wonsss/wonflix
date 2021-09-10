add983e0be536797fba30e1cda5a72c3f43cd4c5
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-10 11:05:16 +0900

    6.2 TVPresenter and Loader Components

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,9 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+import Loader from "Components/Loader";
 
-const TVPresenter = ({topRated, popular, airingToday, error, loading}) => null; 
-//prop 설정
+const Container = styled.div`
+    padding: 0px 10px;
+`
+
+const TVPresenter = ({topRated, popular, airingToday, error, loading}) => (loading ? <Loader/> : 
+    <Container>
+        {topRated && topRated.length>0 && 
+            <Section title="Top Rated Shows">{topRated.map(show => <span key={show.id}>{show.name}</span>)}</Section>}
+        {popular && popular.length>0 && 
+            <Section title="Popular Shows">{popular.map(show => <span key={show.id}>{show.name}</span>)}</Section>}
+        {airingToday && airingToday.length>0 && 
+            <Section title="Airing Today Shows">{airingToday.map(show => <span key={show.id}>{show.name}</span>)}</Section>}
+    </Container>    
+); 
+
 
 TVPresenter.propTypes = {
     topRated: PropTypes.array, popular: PropTypes.array, airingToday: PropTypes.array, error: PropTypes.string, loading: PropTypes.bool.isRequired
